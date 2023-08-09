c2607084a1da8f135c0384762b92101ef73e9448
ViniSouza32 <134454570+ViniSouza32@users.noreply.github.com>
2023-08-08 22:45:23 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import promptSync from "prompt-sync";
 const prompt = promptSync();
 
+let livros: { nome: string; autor: string; qtdePag: string }[] = [];
+
 function cadastroLivros(
   nome: string,
   autor: string,
@@ -13,19 +15,78 @@ function cadastroLivros(
   };
 }
 
-let livros: { nome: string; autor: string; qtdePag: string }[] = [];
+function cadastrarLivro () {
+livros.push(
+        cadastroLivros(
+          prompt("Digite o nome do livro"),
+          prompt("Digite o autor do livro"),
+          prompt("Número de paginas do livro")
+        )
+      );
+
+      console.log("");
+}
+
+function exibirLivro() {
+  let livroExibir: string = prompt(
+    "Digite o nome do livro que deseja procurar"
+  );
+
+  for (let i = 0; i < livros.length; i++) {
+    if (livros[i].nome == livroExibir) {
+      console.log(livros[i]);
+    }
+  }
+
+  console.log("");
+}
+
+function editarLivro() {
+  let livroProcurar: string = prompt(
+        "Digite o nome do livro que deseja procurar"
+      );
+
+      for (let i = 0; i < livros.length; i++) {
+        if (livros[i].nome == livroProcurar) {
+          livros[i].nome = prompt("Digite o nome do livro");
+          livros[i].autor = prompt("Digite o nome do autor(a)");
+          livros[i].qtdePag = prompt("Digite a quantidade de páginas do livro");
+        }
+      }
+
+      console.log("");
+}
+
+function excluirLivro() {
+  let livroProcurar2: string = prompt(
+    "Digite o nome do livro que deseja excluir"
+  );
+
+  for (let i = 0; i < livros.length; i++) {
+    if (livros[i].nome == livroProcurar2) {
+      livros.splice(i, 1);
+    }
+  }
+
+  console.log("");
+}
 
 function catalogoLivros() {
   for (let i = 0; i < livros.length; i++) {
     console.log(
-      "Nome: " + livros[i].nome + " | Autor: " + livros[i].autor + " | Páginas: " + livros[i].qtdePag
+      "Nome: " +
+        livros[i].nome +
+        " | Autor: " +
+        livros[i].autor +
+        " | Páginas: " +
+        livros[i].qtdePag
     );
   }
 }
 
 let opcao: number = 10;
 
-while (opcao != 0) {
+do {
   console.log(
     "1 - Cadastrar livro\n" +
       "2 - Exibir livro\n" +
@@ -39,63 +100,24 @@ while (opcao != 0) {
 
   switch (opcao) {
     case 1:
-      livros.push(
-        cadastroLivros(
-          prompt("Digite o nome do livro"),
-          prompt("Digite o autor do livro"),
-          prompt("Número de paginas do livro")
-        )
-      );
-
-      console.log("");
+      cadastrarLivro();
       break;
     case 2:
-      let livroExibir: string = prompt(
-        "Digite o nome do livro que deseja procurar"
-      );
-
-      for (let i = 0; i < livros.length; i++) {
-        if (livros[i].nome == livroExibir) {
-          console.log(livros[i]);
-        }
-      }
-
-      console.log("");
+      exibirLivro();
       break;
     case 3:
-      let livroProcurar: string = prompt(
-        "Digite o nome do livro que deseja procurar"
-      );
-
-      for (let i = 0; i < livros.length; i++) {
-        if (livros[i].nome == livroProcurar) {
-          livros[i].nome = prompt("Digite o nome do livro");
-          livros[i].autor = prompt("Digite o nome do autor(a)");
-          livros[i].qtdePag = prompt("Digite a quantidade de páginas do livro");
-        }
-      }
-
-      console.log("");
+      editarLivro();
       break;
     case 4:
-      let livroProcurar2: string = prompt(
-        "Digite o nome do livro que deseja excluir"
-      );
-
-      for (let i = 0; i < livros.length; i++) {
-        if (livros[i].nome == livroProcurar2) {
-          livros.splice(i, 1);
-        }
-      }
-
-      console.log("");
+      excluirLivro();
       break;
     case 5:
       catalogoLivros();
-      console.log("");
       break;
     case 0:
       console.log("Tchau! ");
       break;
+    default:
+      break;
   }
-}
+} while (opcao != 0)
